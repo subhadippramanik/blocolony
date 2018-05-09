68d6245136d3f0e0c188274712eab41f8cfa3c69
Subhadip Pramanik <pramaniksubhadip@gmail.com>
2018-05-09 10:07:08 +0530

    refact: use cluster for performance improve

--- app.js
+++ app.js
@@ -3,38 +3,41 @@ var Block = require('./block');
 var BlockChain = require('./chain');
 var uuidv1 = require('uuid/v1');
 var express = require('express');
-var defer = require('node-promise').defer;
+var cluster = require('cluster');
 
 const deviceChain = new BlockChain();
+if (cluster.isMaster) {
+    cluster.fork();
+} else {
+    var app = express();
+    app.use(express.json());
 
-var app = express();
-app.use(express.json());
+    app.post('/device', (req, res) => {
+        console.log("Request for device registration.." + cluster.worker.id);
+        let id = req.body.id;
+        if (deviceChain.hasBlockForDeviceId(id)) {
+            res.status(409);
+        } else {
+            res.status(201);
+            res.send({ status: 'queued' });
+            registerDevice(id);
+        }
+        res.end();
+    });
 
-app.post('/device', (req, res) => {
-    console.log("Request for device registration..");
-    let id = req.body.id;
-    if(deviceChain.hasBlockForDeviceId(id)) {
-        res.status(409);
-    } else {
-        res.status(201);
-        res.send({ status: 'queued' });
-        registerDevice(id);
+    app.get('/chain', (req, res) => {
+        console.log("Request for chain.." + cluster.worker.id);
+        res.json(deviceChain.getChain());
+        res.status(200);
+    });
+
+    app.listen(8080, () => {
+        deviceChain.init();
+    });
+
+    function registerDevice(id) {
+        var device1 = new Device(id);
+        var block1 = new Block(Date.now(), device1);
+        deviceChain.addBlock(block1);
     }
-    res.end();
-});
-
-app.get('/chain', (req, res) => {
-    console.log("Request for chain..");
-    res.json(deviceChain.getChain());
-    res.status(200);
-});
-
-app.listen(8080, () => {
-    deviceChain.init();
-});
-
-function registerDevice(id) {
-    var device1 = new Device(id);
-    var block1 = new Block(Date.now(), device1);
-    deviceChain.addBlock(block1);
 }
